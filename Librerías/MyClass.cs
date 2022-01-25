4aab3e68f22c35c1c082ce2b8a203c310ccb6af4
Juan Gómez Villa <98175871+JuanGomezVilla@users.noreply.github.com>
2022-01-25 15:59:51 +0100

    Add files via upload

---
+++ Librerías/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysRead {
+	public class ArrayRead {
+		public static void imprimirLista(int[] lista){
+			for(int i=0; i<lista.Length; i++){
+				Console.Write(lista[i]);
+				if(i != lista.Length - 1){
+					Console.Write(", ");
+				}
+			}
+			Console.WriteLine();
+		}
+	}
+}
